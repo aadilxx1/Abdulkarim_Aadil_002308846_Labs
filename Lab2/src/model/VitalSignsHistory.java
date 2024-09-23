823c45163ebfe6d16624845cfe3d029beb9563a5
aadilkarim <aadilkarim@localhost>
2024-09-23 02:19:38 -0400

    commit

---
+++ Lab2/src/model/VitalSignsHistory.java
@@ -0,0 +1,35 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+import java.util.ArrayList;
+
+/**
+ *
+ * @author aadilkarim
+ */
+public class VitalSignsHistory {
+    ArrayList<VitalSigns> history;
+    
+    public VitalSignsHistory () {
+      history = new ArrayList<VitalSigns>();
+    }
+    
+    public VitalSigns addNewVitals () {
+        VitalSigns newVS = new VitalSigns ();
+        history.add(newVS);
+        return newVS;
+    
+    }
+    
+    public void removeVitals (VitalSigns vs) {
+        history.remove(vs);
+    
+    }
+    
+    public ArrayList<VitalSigns> getHistory () {
+    return history;
+    }
+}
