9404d2629def290a0e43d5b40a0e85692500a8c9
aadilkarim <aadilkarim@localhost>
2024-09-24 14:51:48 -0400

    Lab2

---
+++ Lab1_Person/src/model/Person.java
@@ -0,0 +1,195 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author aadilkarim
+ */
+public class Person {
+
+    public String getFname() {
+        return fname;
+    }
+
+    public void setFname(String fname) {
+        this.fname = fname;
+    }
+
+    public String getLname() {
+        return lname;
+    }
+
+    public void setLname(String lname) {
+        this.lname = lname;
+    }
+
+    public String getAge() {
+        return age;
+    }
+
+    public void setAge(String age) {
+        this.age = age;
+    }
+
+    public String getGender() {
+        return gender;
+    }
+
+    public void setGender(String gender) {
+        this.gender = gender;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public String getdLno() {
+        return dLno;
+    }
+
+    public void setdLno(String dLno) {
+        this.dLno = dLno;
+    }
+
+    public String getdLstate() {
+        return dLstate;
+    }
+
+    public void setdLstate(String dLstate) {
+        this.dLstate = dLstate;
+    }
+
+    public String getSsn() {
+        return ssn;
+    }
+
+    public void setSsn(String ssn) {
+        this.ssn = ssn;
+    }
+
+    public String getNationality() {
+        return nationality;
+    }
+
+    public void setNationality(String nationality) {
+        this.nationality = nationality;
+    }
+
+    public String getPet() {
+        return pet;
+    }
+
+    public void setPet(String pet) {
+        this.pet = pet;
+    }
+
+    public String getFavsport() {
+        return favsport;
+    }
+
+    public void setFavsport(String favsport) {
+        this.favsport = favsport;
+    }
+
+    public String getFavmovie() {
+        return favmovie;
+    }
+
+    public void setFavmovie(String favmovie) {
+        this.favmovie = favmovie;
+    }
+
+    public String getAddl1() {
+        return addl1;
+    }
+
+    public void setAddl1(String addl1) {
+        this.addl1 = addl1;
+    }
+
+    public String getAddl2() {
+        return addl2;
+    }
+
+    public void setAddl2(String addl2) {
+        this.addl2 = addl2;
+    }
+
+    public String getCity() {
+        return city;
+    }
+
+    public void setCity(String city) {
+        this.city = city;
+    }
+
+    public String getState() {
+        return state;
+    }
+
+    public void setState(String state) {
+        this.state = state;
+    }
+
+    public String getZip() {
+        return zip;
+    }
+
+    public void setZip(String zip) {
+        this.zip = zip;
+    }
+
+    public String getTele() {
+        return tele;
+    }
+
+    public void setTele(String tele) {
+        this.tele = tele;
+    }
+
+    public String getFax() {
+        return fax;
+    }
+
+    public void setFax(String fax) {
+        this.fax = fax;
+    }
+    
+    String fname;
+    String lname;
+    String age;
+    String gender;
+    String email;
+    String phone;
+    String dLno;
+    String dLstate;
+    String ssn;
+    String nationality;
+    String pet;
+    String favsport;
+    String favmovie;
+    String addl1;
+    String addl2;
+    String city;
+    String state;
+    String zip;
+    String tele;
+    String fax;
+        
+    
+}
