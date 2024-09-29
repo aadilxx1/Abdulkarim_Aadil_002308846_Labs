7884438d36fec7617e5e80d1fa4273c5725ae195
aadilkarim <aadilkarim@localhost>
2024-09-29 16:18:29 -0400

    lab3 1st commit

---
+++ Lab_3/src/model/Account.java
@@ -0,0 +1,55 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package model;
+
+/**
+ *
+ * @author aadilkarim
+ */
+public class Account {
+    
+    private String bankName;
+    private String accountNo;
+    private String routingNo;
+    private int balance;
+
+    public String getBankName() {
+        return bankName;
+    }
+
+    public void setBankName(String bankName) {
+        this.bankName = bankName;
+    }
+
+    public String getAccountNo() {
+        return accountNo;
+    }
+
+    public void setAccountNo(String accountNo) {
+        this.accountNo = accountNo;
+    }
+
+    public String getRoutingNo() {
+        return routingNo;
+    }
+
+    public void setRoutingNo(String routingNo) {
+        this.routingNo = routingNo;
+    }
+
+    public int getBalance() {
+        return balance;
+    }
+
+    public void setBalance(int balance) {
+        this.balance = balance;
+    }
+    
+    @Override
+    public String toString() {
+      return bankName;
+    }
+    
+}
